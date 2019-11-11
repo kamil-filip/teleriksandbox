811976a2397006ecb4375479d48849117928a392
kamil-filip <kamilfkozlowski@gmail.com>
2019-11-11 20:16:44 +0100

    menus

---
+++ MyWPFSandbox/MainViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MyWPFSandbox
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public MainViewModel()
+        {
+            InitMenuItems();
+            InitUserToolbarItems();
+            InitAdminToolbarItems();      
+        }
+
+        private void InitAdminToolbarItems()
+        {
+            AdminToolbarItems = new ObservableCollection<BaseMenuItemViewModel>()
+            {
+                new BaseMenuItemViewModel("Admin")
+            };
+        }
+
+        private void InitUserToolbarItems()
+        {
+            var toolbarCommand = new DelegateCommand(x =>
+            {
+                Console.WriteLine(x);
+            });
+
+            UserToolbarItems = new ObservableCollection<BaseMenuItemViewModel>()
+            {
+                new BaseMenuItemViewModel("One",toolbarCommand),
+                new BaseMenuItemViewModel("Two",toolbarCommand, false),
+                new BaseMenuItemViewModel("Three",toolbarCommand)
+            };
+        }
+
+        private void InitMenuItems()
+        {
+
+            Action a = () =>
+            {
+                MessageBox.Show("FX");
+            };
+
+
+            Action b = () =>
+            {
+                MessageBox.Show("Options");
+            };
+
+            var menuCommand = new DelegateCommand(x =>
+                {
+                    var vm = ((BaseMenuItemViewModel)x);
+
+                    if(vm.Header == "FXReadonly" && vm.IsChecked)
+                    {
+                        a();
+                    }
+                    if(vm.Header == "OptionsReadOnly")
+                    {
+                        b();
+                    }
+
+
+                    Console.WriteLine(x);
+                });
+
+
+            MenuItems = new ObservableCollection<BaseMenuItemViewModel>()
+            {
+                new BaseMenuItemViewModel("File"){
+                    SubItems = new ObservableCollection<BaseMenuItemViewModel>()
+                    {
+                        new BaseMenuItemViewModel("Exit")
+                    }
+                },
+                new BaseMenuItemViewModel("Mode"){
+                    SubItems = new ObservableCollection<BaseMenuItemViewModel>()
+                    {
+                        new BaseMenuItemViewModel("FXReadonly", menuCommand){IsCheckable = true, IsChecked = false },
+                        new BaseMenuItemViewModel("OptionsReadOnly",menuCommand){IsCheckable = true,IsChecked = true},
+                        new BaseMenuItemViewModel("IndicesReadonly",menuCommand, false){IsCheckable = true,IsChecked = true},
+                    }
+                },
+                new BaseMenuItemViewModel("Help"){
+                    SubItems = new ObservableCollection<BaseMenuItemViewModel>()
+                    {
+                        new BaseMenuItemViewModel("About"){}
+                    }
+                }
+            };
+        }
+
+        public ObservableCollection<BaseMenuItemViewModel> MenuItems { get; set; }
+        public ObservableCollection<BaseMenuItemViewModel> UserToolbarItems { get; set; }
+        public ObservableCollection<BaseMenuItemViewModel> AdminToolbarItems { get; set; }
+    }
+
+
+
+
+}
---
+++ MyWPFSandbox/MenuItemViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MyWPFSandbox
+{
+    public class BaseMenuItemViewModel : ViewModelBase
+    {
+
+        public BaseMenuItemViewModel(string header,ICommand onSelectCommand = null, bool isEnabled = true)
+        {
+            Header = header;
+            OnSelectCommand = onSelectCommand;
+            IsEnabled = isEnabled;
+            SubItems = new ObservableCollection<BaseMenuItemViewModel>();
+        }
+
+        public bool IsCheckable { get; set; }
+
+        private bool _isChecked;
+        public bool IsChecked //{ get; set; }
+        {
+            get { return _isChecked; }
+            set {
+               _isChecked = value;
+                OnPropertyChanged();
+            } 
+        }
+
+        public bool IsEnabled
+        {
+            get; set;
+        } 
+
+
+        public string Header { get; set; }
+        public ICommand OnSelectCommand { get; set; }
+
+        public ObservableCollection<BaseMenuItemViewModel> SubItems { get; set; }
+    }
+}
